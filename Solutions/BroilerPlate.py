09c42fe8c0bd34c03bfc07098a069e1f99518172
kartik-chindarkar <kartik.chindarkar@sjsu.edu>
2024-11-01 13:53:10 -0700

    valid anagram

---
+++ Solutions/BroilerPlate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def methodName(self, params):
+        pass
+
+# Create an instance of the class
+sol = Solution()
+
+# Call the method on the test cases
+result = sol.methodName("params")
+print(result)
+
+## Try efficient solution
---
+++ Solutions/ValidAnagram.py
@@ -0,0 +1,25 @@
+class Solution:
+    def ValidAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+
+        return True
+
+# Create an instance of the class
+sol = Solution()
+
+# Call the method on the instance
+result = sol.ValidAnagram("anagram","gramana")
+print(result)
+
+## Try efficient solution
