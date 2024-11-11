7a17fc490234f5009d78e2c56985999c4ab1738a
kartik-chindarkar <kartik.chindarkar@sjsu.edu>
2024-11-10 23:29:46 -0800

    Solution

---
+++ Solutions/Arrays & Hashing/ValidateSubsequence.py
@@ -0,0 +1,11 @@
+def isValidSubsequence(array, sequence):
+    j=0
+
+    for i in range(len(array)):
+        if j < len(sequence) and array[i] == sequence[j]:
+            j += 1
+    return j == len(sequence)
+
+array = [5,1,22,25,6,-1,8,10]
+sequence = [1,6,-1,10]
+print(isValidSubsequence(array, sequence)) 
