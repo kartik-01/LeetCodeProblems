4e8759c99fb94aff8a0431076e824d6d805e3ae2
kartik-01 <karthikchindarkar@gmail.com>
2025-01-30 19:53:03 -0800

    LCS

---
+++ Solutions/1 - Arrays & Hashing/8LCS.py
@@ -0,0 +1,24 @@
+class Solution:
+    def lcs(self, nums):
+        numSet = set(nums)
+        longest = 0
+        
+        for n in nums:
+            if (n-1) not in nums:
+                length = 0
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        
+        return longest
+        
+        pass
+
+# Create an instance of the class
+sol = Solution()
+
+# Call the method on the test cases
+result = sol.lcs([100, 4, 200, 1, 3, 2])
+print(result)
+
+## Try efficient solution
--- Solutions/Practice/Demo.py
+++ Solutions/Practice/Demo.py
@@ -2,18 +2,5 @@ from collections import defaultdict
 
 countS = defaultdict(list)
 
-countS["a"]=12
-countS[12.3]=13
-
-count = {"a":2,"b":3}
-
-count["c"] = 4
-
-print(countS["a"])
-print(countS[12.3])
-
-
-mystring = "6#params4#test"
-
-print(mystring[1+1:1+1+6])
-
+count = [0] * 26
+print(count)
