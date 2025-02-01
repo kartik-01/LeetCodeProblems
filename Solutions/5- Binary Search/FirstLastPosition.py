214e9a66ad938f740088e842dfe0e551a746d8d5
kartik-01 <karthikchindarkar@gmail.com>
2025-01-31 16:00:43 -0800

    Binary Search Problems

--- Solutions/5- Binary Search/FirstLastPosition.py
+++ Solutions/5- Binary Search/FirstLastPosition.py
@@ -2,6 +2,36 @@
 
 class Solution:
     def searchRange(self, nums, target):
+        def search(nums, target, isFirstIndex):
+            ans = - 1
+            start = 0
+            end = len(nums) - 1
+
+            while start <= end:
+                mid = start + (end - start) // 2
+
+                if target < nums[mid]:
+                    end = mid - 1
+                elif target > nums[mid]:
+                    start = mid + 1
+                else:
+                    # potential ans has been found
+                    ans = mid
+                    if isFirstIndex:
+                        end = mid - 1
+                    else:
+                        start = mid + 1
+            return ans
+        
+        ans = [-1,-1]
+        
+        start = search(nums, target, isFirstIndex=True)
+        end = search(nums, target, isFirstIndex=False)
+
+        ans[0] = start
+        ans[1] = end
+
+        return ans
         pass
 
 # Create an instance of the class
