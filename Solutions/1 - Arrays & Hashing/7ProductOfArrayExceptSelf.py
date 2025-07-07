713da9288e622f57340a6163cddede1c494a60dd
kartik-01 <karthikchindarkar@gmail.com>
2025-07-07 13:13:23 -0700

    update

--- Solutions/1 - Arrays & Hashing/7ProductOfArrayExceptSelf.py
+++ Solutions/1 - Arrays & Hashing/7ProductOfArrayExceptSelf.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/product-of-array-except-self/description/
 def productExceptSelf(nums):
+    # Time complexity O(n), Space Complexity O(n)
     n = len(nums)
     prefix, postfix, result = [1] * n, [1] * n, [1] * n
 
@@ -27,3 +28,24 @@ def productExceptSelf(nums):
 # Example usage:
 nums = [-1,1,0,-3,3]
 print(productExceptSelf(nums))  # Output: [24, 12, 8, 6]
+
+## Try efficient solution
+# Time complexity O(n), Space Complexity O(1)
+
+def productExceptSelf2(nums):
+    res = [1] * len(nums)
+
+    prefix=1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
+        
+    return res
+
+nums = [1,2,3,4]
+print(productExceptSelf2(nums))  # Output: [24, 12, 8, 6]
