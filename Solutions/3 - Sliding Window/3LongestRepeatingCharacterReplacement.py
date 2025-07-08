269b8f177dabec7d58c15e71f23f9d53507876c4
kartik-01 <karthikchindarkar@gmail.com>
2025-07-08 16:31:53 -0700

    update

--- Solutions/3 - Sliding Window/3LongestRepeatingCharacterReplacement.py
+++ Solutions/3 - Sliding Window/3LongestRepeatingCharacterReplacement.py
@@ -1,14 +1,27 @@
 class Solution:
     # Bruteforce
     def characterReplacement(self, s, k):
-        
+        l = 0
+        count = {}
+        res = 0
+        maxF = 0
+
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxF = max(maxF, count[s[r]])
+            while (r-l + 1) - maxF > k:
+                count[s[l]] -= 1
+                l+=1
+            res=max(res, (r-l+1))
+
+        return res       
         pass
 
 # Create an instance of the class
 sol = Solution()
 
 # Call the method on the test cases
-result = sol.characterReplacement('ABAB', 2)
+result = sol.characterReplacement('ABABBA', 2)
 print(result)
 
 ## Try efficient solution
