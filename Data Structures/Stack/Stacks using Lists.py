b214e7b0666ca30ba81d608b38ce6af65e289790
Kartik <karthikchindarkar@gmail.com>
2026-02-14 11:59:57 -0800

    update

---
+++ Data Structures/Stack/Stacks using Lists.py
@@ -0,0 +1,38 @@
+class Stack:
+    def __init__(self):
+        self.values = []
+
+    def is_empty(self):
+        return len(self.values) == 0
+
+    def push(self, x):
+        self.values.append(x)
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Cannot pop from an empty stack.")
+        return self.values.pop()
+        
+    def peek(self):
+        if not self.is_empty():
+            return self.values[-1]
+        return None
+
+    def size(self):
+        return len(self.values)
+
+my_stack = Stack()
+
+print(my_stack.pop())
+print(my_stack.peek())
+print(my_stack.size())
+
+print(my_stack.push(1))
+my_stack.push(2)
+my_stack.push(3)
+my_stack.push(4)
+my_stack.push(5)
+
+my_stack.pop()
+print(my_stack.values)
+print(my_stack.peek())
--- Solutions/5- Binary Search/OrderAgnosticBS.py
+++ Solutions/5- Binary Search/OrderAgnosticBS.py
@@ -1,26 +1,27 @@
 class Solution:
     def orderAgnosticbinarySearch(self, nums, target):
-        start = 0
-        end = len(nums) - 1
+        l = 0
+        r = len(nums) - 1
 
-        isOrderAsc = True if nums[start] < nums[end] else False
+        isOrderAsc = True if nums[l] < nums[r] else False
 
-        while start <= end:
-            mid = start + (end - start) // 2
+        while l <= r:
+            mid = (l+r) // 2
 
             if nums[mid] == target:
                 return mid
-
+            
             elif isOrderAsc:
-                if target  < nums[mid]:
-                    end = mid - 1
+                if target < nums[mid]:
+                    r = mid - 1
                 else:
-                    start = mid + 1
+                    l = mid + 1
             else:
-                if target  < nums[mid]:
-                    start = mid + 1
+                if target < nums[mid]:
+                    l = mid + 1
                 else:
-                    end = mid - 1
+                    r = mid - 1
+        
         return -1
 
         pass
@@ -31,5 +32,8 @@ sol = Solution()
 # Call the method on the test cases
 result = sol.orderAgnosticbinarySearch([45,30,21,20,15,5,3], 5)
 print(result)
+
+result2 = sol.orderAgnosticbinarySearch([3,5,15,20,21,30,45], 5)
+print(result2)
 
 ## Try efficient solution
